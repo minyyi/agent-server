bc1be1edf89a7508c692b95ef78c5e895b36fe45
minyyi <117286294+minyyi@users.noreply.github.com>
2025-08-05 11:28:13 +0900

    initial

---
+++ src/main/java/com/example/agent_server/AppController.java
@@ -0,0 +1,34 @@
+package com.example.agent_server;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class AppController {
+    
+    private BoardRepository boardRepository;
+
+    public AppController(BoardRepository boardRepository) {
+        this.boardRepository = boardRepository;
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home Action!";
+    }
+    
+    @GetMapping("/boards")
+    public ResponseEntity<List<Board>> getBoards() {
+        Board board = new Board("agent", "agent content");
+        boardRepository.save(board);
+
+        List<Board> boards = boardRepository.findAll();
+        return  ResponseEntity.ok().body(boards);
+    }
+    
+}
